e0b4680e9babb2a518f809f644c3b7c7d775cc49
CoderSid28 <siddharthmishra457@gmail.com>
2025-10-29 20:34:02 +0530

    edit

--- list ,key and Hooks/useSelector and useDispatch. example/my-app/src/Counter.jsx
+++ list ,key and Hooks/useSelector and useDispatch. example/my-app/src/Counter.jsx
@@ -3,14 +3,15 @@ import { useSelector, useDispatch } from "react-redux";
 
 function Counter() {
   const count = useSelector((state) => state.count); 
+  const dispatch = useDispatch(); // ✅ Get dispatch function
 
   return (
-    <div>
+    <div style={{ textAlign: "center", marginTop: "30px" }}>
       <h2>Count: {count}</h2>
       <button onClick={() => dispatch({ type: "INCREMENT" })}>
         Increment
       </button>
-      <button onClick={() => dispatch({ type: "DECREMENT" })}>
+      <button onClick={() => dispatch({ type: "DECREMENT" })} style={{ marginLeft: "10px" }}>
         Decrement
       </button>
     </div>
